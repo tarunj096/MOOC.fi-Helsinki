065250448d48230a874b4abff0df2beac2475b38
tarunj096 <58319215+tarunj096@users.noreply.github.com>
2020-08-12 18:20:16 +0530

    Added Files

---
+++ mooc_java_programming_PART_3/part03-Part03_07.RememberTheseNumbers/src/main/java/RememberTheseNumbers.java
@@ -0,0 +1,27 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RememberTheseNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while (true) {
+            int luku = Integer.valueOf(scanner.nextLine());
+            if (luku == -1) {
+                break;
+            }
+
+            numbers.add(luku);
+        }
+        int index = 0;
+        int i = numbers.size();
+        while(index<i){
+            System.out.println(numbers.get(index));
+            index++;
+        }
+
+    }
+}
