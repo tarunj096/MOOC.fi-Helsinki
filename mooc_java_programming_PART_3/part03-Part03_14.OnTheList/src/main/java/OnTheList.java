d33fde66d6f0322cf0ab6670ade90765755f436f
tarunj096 <58319215+tarunj096@users.noreply.github.com>
2020-08-16 03:02:53 +0530

    Added Files

---
+++ mooc_java_programming_PART_3/part03-Part03_14.OnTheList/src/main/java/OnTheList.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class OnTheList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+        }
+        System.out.print("Search for? ");
+        String s = scanner.nextLine();
+        int si = list.size()-1;
+        if (list.contains(s)) {
+            System.out.println(s+" was found!");
+        } else{
+           System.out.println(s+" was not found!");
+       }
+    }
+}
