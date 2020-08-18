4795006699114b491511ad785c66e1031291c276
tarunj096 <58319215+tarunj096@users.noreply.github.com>
2020-08-18 23:13:44 +0530

    Added Files

---
+++ mooc_java_programming_PART_3/part03-Part03_18.Swap/src/main/java/Swap.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class Swap {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 3;
+        array[2] = 5;
+        array[3] = 7;
+        array[4] = 9;
+
+        int index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+        System.out.println("");
+
+        // Implement here
+        // asking for the two indices
+        // and then swapping them
+        int start = 0,end = 0;
+        System.out.println("Give two indices to swap:");
+        start = Integer.valueOf(scanner.nextLine());
+        end = Integer.valueOf(scanner.nextLine());
+        int temp = array[start];
+        array[start] = array[end];
+        array[end] = temp;
+        System.out.println("");
+        index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+    }
+
+}
