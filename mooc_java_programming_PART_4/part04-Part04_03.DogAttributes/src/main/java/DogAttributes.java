c62ea22f1f9b2a98d787272ab365a584eb34d6ac
Tarun Kumar J <58319215+tarunj096@users.noreply.github.com>
2020-09-19 01:27:52 +0530

    Added Files

---
+++ mooc_java_programming_PART_4/part04-Part04_03.DogAttributes/src/main/java/DogAttributes.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class DogAttributes {
+       
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
