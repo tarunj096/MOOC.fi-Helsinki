dbe27217f6059e3fa0af3e25d99b9a0ded729c74
Tarun Kumar J <58319215+tarunj096@users.noreply.github.com>
2020-10-13 14:36:51 +0530

    Added Files

---
+++ mooc_java_programming_PART_4/part04-Part04_11.Film/src/main/java/Film.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tarunj096
+ */
+public class Film {
+    private String name;
+    private int ageRating;
+    
+    public Film(String filmName, int filmAgeRating){
+        this.name = filmName;
+        this.ageRating = filmAgeRating;
+    }
+    public String name(){
+        return name;
+    }
+    public int ageRating(){
+        return ageRating;
+    }
+}
